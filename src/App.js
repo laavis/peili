e8bb7d6a2f86153ec2073076d09c3410eeb29465
Tuomas Pöyry <tuomas@munkkeli.io>
2019-10-30 14:26:52 +0200

    React router & simple header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-  const App = () => (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-  );
-  
+import { Header } from './components/Header';
+import { SpecialistDashboard } from './views/SpecialistDashboard';
+
+const App = () => (
+  <Router>
+    <div>
+      <Header />
+
+      <Switch>
+        <Route path="/test">
+          <div>This is a test page.</div>
+        </Route>
+        <Route path="/">
+          <SpecialistDashboard />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+
 export default App;
